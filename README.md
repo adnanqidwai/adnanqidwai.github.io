99030ffc3be2dd2c3a0419012ec4abf02892eb5b
adnanqidwai <adgta25@gmail.com>
2025-01-04 23:47:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to my github page!
+
+I will be updating this on a not-so-regular basis😅.
---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://adnanqidwai.github.io',
+  base: '/',
+  build: {
+    assets: 'assets'
+  }
+});
